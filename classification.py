2a4d0af1e062748b2cf6c4df5a71fd10bee5b8b7
Sander Coates <sandercoates@gmail.com>
2020-01-25 13:34:19 +0000

    moved classification helper functions to avoid nested functions and ease testing

--- classification.py
+++ classification.py
@@ -32,6 +32,62 @@ class DecisionTreeClassifier(object):
     def __init__(self):
         self.is_trained = False
 
+    def find_best_node(self, feature_arr, label_arr, number_splits=1):
+
+        assert (number_splits == 1)  # Only binary split implemented so far
+
+        best_feature = 'n.a.'
+        max_gain = -1
+        thresholds = []
+
+        # Calculate parent entropy
+        parent_entropy = ep.parent_entropy([feature_arr, label_arr])
+
+        # Iterate over features (rows of transposed feature_set)
+        for feature in range(feature_arr.shape[1]):
+            # Iterate over possible thresholds
+            for threshold in np.unique(feature_arr[:, feature]):
+                children_subsets = self.split_dataset([feature_arr, label_arr], feature, [threshold])
+                info_gain = parent_entropy - ep.child_entropy(children_subsets, feature_arr.shape[0])
+                if info_gain > max_gain:
+                    best_feature = feature  # TODO Consider moving out of inner for loop
+                    max_gain = info_gain
+                    thresholds = [threshold]
+
+        assert (max_gain >= 0)  # Assert best_feature and threshold were changed
+        return best_feature, thresholds
+
+    # Thresholds is array of threshold for which all less than or equal to threshold value will be put in set
+    # "left" of split
+    def split_dataset(self, parent_set, feature, thresholds):
+        feature_arr = parent_set[0]
+        label_arr = parent_set[1]
+        assert feature_arr.shape[0] == label_arr.shape[0]
+
+        # Prepare empty nested arrays for children subsets
+        children_subsets = [[[], []] for _ in range(len(thresholds) + 1)]
+
+        # Copy parent dataset rows into appropriate children datasets
+        thresholds.sort()
+        for row in range(len(feature_arr)):
+            copied = False
+            for split in range(len(thresholds)):
+                if feature_arr[row][feature] <= thresholds[split]:
+                    children_subsets[split][0].append(feature_arr[row])
+                    children_subsets[split][1].append(label_arr[row])
+                    copied = True
+                    break
+            if not copied:  # Value of node is not below any threshold for the row, append to last children subset
+                children_subsets[-1][0].append(feature_arr[row])
+                children_subsets[-1][1].append(label_arr[row])
+
+        # Convert feature and label arrays to Numpy array type
+        for split in range(len(thresholds) + 1):
+            children_subsets[split][0] = np.asarray(children_subsets[split][0], parent_set[0].dtype)
+            children_subsets[split][1] = np.asarray(children_subsets[split][1], parent_set[1].dtype)
+
+        return children_subsets
+
     def train(self, x, y):
         """ Constructs a decision tree classifier from data
         
@@ -59,60 +115,6 @@ class DecisionTreeClassifier(object):
         #######################################################################
 
         # Own
-
-        def find_best_node(feature_arr, label_arr, number_splits=2):
-
-            assert (number_splits == 2)  # Only binary split implemented so far
-
-            best_node = 'n.a.'
-            max_gain = -1
-            thresholds = []
-
-            # Iterate over features (rows of transposed feature_set)
-            for node in range(feature_arr.shape[1]):
-                # Iterate over possible thresholds
-                for threshold in feature_arr[:, node]:  # TODO Iterate over unique values only
-                    children_subsets = split_dataset([feature_arr, label_arr], node, [threshold])
-                    # TODO Assert parent entropy is only calculated once
-                    info_gain = ep.information_gain([feature_arr, label_arr], children_subsets)
-                    if info_gain > max_gain:
-                        best_node = node  # TODO Consider moving out of inner for loop
-                        max_gain = info_gain
-                        thresholds = [threshold]
-
-            assert (max_gain >= 0)  # Assert best_node and threshold were changed
-            return best_node, thresholds
-
-        # Thresholds is array of threshold for which all less than or equal to threshold value will be put in set
-        # "left" of split
-        def split_dataset(self, parent_set, node, thresholds):
-            feature_arr = parent_set[0]
-            label_arr = parent_set[1]
-            assert feature_arr.shape[0] == label_arr.shape[0]
-
-            # Prepare empty nested arrays for children subsets
-            children_subsets = [[[], []] for _ in range(len(thresholds) + 1)]
-
-            # Copy parent dataset rows into appropriate children datasets
-            thresholds.sort()
-            for row in range(len(feature_arr)):
-                copied = False
-                for split in range(len(thresholds)):
-                    if feature_arr[row][node] <= thresholds[split]:
-                        children_subsets[split][0].append(feature_arr[row])
-                        children_subsets[split][1].append(label_arr[row])
-                        copied = True
-                        break
-                if not copied:  # Value of node is not below any threshold for the row, append to last children subset
-                    children_subsets[-1][0].append(feature_arr[row])
-                    children_subsets[-1][1].append(label_arr[row])
-
-            # Convert feature and label arrays to Numpy array type
-            for split in range(len(thresholds) + 1):
-                children_subsets[split][0] = np.asarray(children_subsets[split][0], parent_set[0].dtype)
-                children_subsets[split][1] = np.asarray(children_subsets[split][1], parent_set[1].dtype)
-
-            return children_subsets
 
         # set a flag so that we know that the classifier has been trained
         self.is_trained = True
